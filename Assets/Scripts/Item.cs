70fc65481a9cb5e9999403344b5f35b4c02fed6c
andrescrd <c.andresricardo@gmail.com>
2021-02-28 08:51:07 -0300

    add box and item

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [SerializeField] private float speed = 150;
+    
+    private Rigidbody m_Rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        if (m_Rigidbody)
+        {
+            m_Rigidbody.AddForce(Vector3.up * speed);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
